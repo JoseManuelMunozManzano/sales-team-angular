eea6b8735b174f02571256c4ef7eb5c767f90887
Neimerc <Neil_Mercury@yahoo.es>
2020-05-17 16:04:30 +0200

    create sample data

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("José", "Muñoz", "aaa@a", 50000),
+    new SalesPerson("Luis", "Perez", "bbb@a", 40000),
+    new SalesPerson("Lidia", "Lopez", "ccc@a", 90000),
+    new SalesPerson("Maria", "Gutierrez", "ddd@a", 60000)
+  ];
+  
 
   constructor() { }
 
